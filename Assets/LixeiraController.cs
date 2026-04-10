63d6488d31166b46408bde46d0c580b464558913
Olavo Menezes Gouvêa <olavotf3@gmail.com>
2026-04-10 08:55:38 -0300

    Limite Lixeira

--- Assets/LixeiraController.cs
+++ Assets/LixeiraController.cs
@@ -17,5 +17,16 @@ public class LixeiraController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         this.transform.position += new Vector3(horizontalInput*velocity,0,0);
+        if(this.transform.position.x<10)
+        {
+            this.transform.position = new Vector3(10,
+                                                    this.transform.position.y,
+                                                    this.transform.position.z);
+        }else if (this.transform.position.x<-10)
+        {
+            this.transform.position = new Vector3(-10,
+                                                    this.transform.position.y,
+                                                    this.transform.position.z);
+        }
     }
 }
