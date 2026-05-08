01c75ab008e45721051c9253543d3644c270777f
polemifamlia <aasdsad>
2026-05-08 16:00:28 -0300

    Som Do Jogo

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class AddPoints : MonoBehaviour
 {
 
+    public AudioSource source;
+
 public LixoSpawnerController lixoSpawnerController;
 
 private void OnCollisionEnter(Collision collision) {
 
 if (collision.gameObject.CompareTag("Lixo")) {
 Destroy(collision.gameObject);
+source.Play();
 lixoSpawnerController.AddToPoints(1);
 }
 }
